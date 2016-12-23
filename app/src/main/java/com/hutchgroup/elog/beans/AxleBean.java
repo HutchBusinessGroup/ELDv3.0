bd43e9cdd115c9be92f1869142e18400d728aef5
HutchBusinessGroup <gary.dhaliwal@hutchbusinessgroup.com>
2016-12-23 18:38:14 +0530

    app.imi

---
+++ app/src/main/java/com/hutchgroup/elog/beans/AxleBean.java
@@ -0,0 +1,13 @@
+package com.hutchgroup.elog.beans;
+
+/**
+ * Created by Deepak on 12/23/2016.
+ */
+
+public class AxleBean {
+    int axleNo, vehicleId, tireNo, axlePosition;
+    boolean doubleTireFg, frontTireFg;
+    double lowPressure, highPressure, pressure, lowTemperature, highTemperature, temperature;
+    String[] sensorIds;
+
+}
