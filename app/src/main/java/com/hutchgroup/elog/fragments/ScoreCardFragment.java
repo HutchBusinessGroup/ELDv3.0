5dd16d91211f6924431b16a6acacee8a5223508f
SAMSUNG <gary.dhaliwal@hutchbusinessgroup.com>
2017-01-13 14:30:49 +0530

    driverScoreCard

--- app/src/main/java/com/hutchgroup/elog/fragments/ScoreCardFragment.java
+++ app/src/main/java/com/hutchgroup/elog/fragments/ScoreCardFragment.java
@@ -12,6 +12,7 @@ import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
+
 import com.hutchgroup.elog.R;
 import com.hutchgroup.elog.adapters.ScoreCardAdapter;
 import com.hutchgroup.elog.adapters.ScoreCardThresholdAdapter;
@@ -36,6 +37,127 @@ public class ScoreCardFragment extends Fragment implements AlertDB.IScoreCard {
     public static boolean IsTesting = false;
 
     Thread thScores;
+    TextView tvDrivingTime;
+
+    TextView tvLowWasherFluid, tvLowWasherFluidPer, tvLowWasherFluidCount;
+    TextView tvWarmUpEngine, tvWarmUpEnginePer, tvWarmUpEngineCount;
+    TextView tvLowEngineOil, tvLowEngineOilPer, tvLowEngineOilCount;
+    TextView tvLowCoolant, tvLowCoolantPer, tvLowCoolantCount;
+    TextView tvHighRPM, tvHighRPMPer, tvHighRPMCount;
+    TextView tvSpeedViolation, tvSpeedViolationPer, tvSpeedViolationCount;
+    TextView tvHardAcceleration, tvHardAccelerationPer, tvHardAccelerationCount;
+    TextView tvHardBreaking, tvHardBreakingPer, tvHardBreakingCount;
+    TextView tvIdling, tvIdlingPer, tvIdlingCount;
+    TextView tvTPMS, tvTPMSPer, tvTPMSCount;
+    TextView tvCriticalWarning, tvCriticalWarningPer, tvCriticalWarningCount;
+    TextView tvChecnkEngineOn, tvChecnkEngineOnPer, tvChecnkEngineOnCount;
+    TextView tvSharpTurnLeft, tvSharpTurnLeftPer, tvSharpTurnLeftCount;
+    TextView tvSharpTurnRight, tvSharpTurnRightPer, tvSharpTurnRightCount;
+    TextView tvHOS, tvHOSPer, tvHOSCount;
+    TextView tvTripInspectionFailure, tvTripInspectionFailurePer, tvTripInspectionFailureCount;
+
+    private void initialize(View view) {
+        tvDrivingTime = (TextView) view.findViewById(R.id.tvDrivingTime);
+
+        tvLowWasherFluid = (TextView) view.findViewById(R.id.tvLowWasherFluid);
+        tvLowWasherFluidPer = (TextView) view.findViewById(R.id.tvLowWasherFluidPer);
+        tvLowWasherFluidCount = (TextView) view.findViewById(R.id.tvLowWasherFluidCount);
+
+        tvWarmUpEngine = (TextView) view.findViewById(R.id.tvWarmUpEngine);
+        tvWarmUpEnginePer = (TextView) view.findViewById(R.id.tvWarmUpEnginePer);
+        tvWarmUpEngineCount = (TextView) view.findViewById(R.id.tvWarmUpEngineCount);
+
+        tvLowEngineOil = (TextView) view.findViewById(R.id.tvLowEngineOil);
+        tvLowEngineOilPer = (TextView) view.findViewById(R.id.tvLowEngineOilPer);
+        tvLowEngineOilCount = (TextView) view.findViewById(R.id.tvLowEngineOilCount);
+
+        tvLowCoolant = (TextView) view.findViewById(R.id.tvLowCoolant);
+        tvLowCoolantPer = (TextView) view.findViewById(R.id.tvLowCoolantPer);
+        tvLowCoolantCount = (TextView) view.findViewById(R.id.tvLowCoolantCount);
+
+        tvHighRPM = (TextView) view.findViewById(R.id.tvHighRPM);
+        tvHighRPMPer = (TextView) view.findViewById(R.id.tvHighRPMPer);
+        tvHighRPMCount = (TextView) view.findViewById(R.id.tvHighRPMCount);
+
+        tvSpeedViolation = (TextView) view.findViewById(R.id.tvSpeedViolation);
+        tvSpeedViolationPer = (TextView) view.findViewById(R.id.tvSpeedViolationPer);
+        tvSpeedViolationCount = (TextView) view.findViewById(R.id.tvSpeedViolationCount);
+
+        tvHardAcceleration = (TextView) view.findViewById(R.id.tvHardAcceleration);
+        tvHardAccelerationPer = (TextView) view.findViewById(R.id.tvHardAccelerationPer);
+        tvHardAccelerationCount = (TextView) view.findViewById(R.id.tvHardAccelerationCount);
+
+        tvHardBreaking = (TextView) view.findViewById(R.id.tvHardBreaking);
+        tvHardBreakingPer = (TextView) view.findViewById(R.id.tvHardBreakingPer);
+        tvHardBreakingCount = (TextView) view.findViewById(R.id.tvHardBreakingCount);
+
+        tvIdling = (TextView) view.findViewById(R.id.tvIdling);
+        tvIdlingPer = (TextView) view.findViewById(R.id.tvIdlingPer);
+        tvIdlingCount = (TextView) view.findViewById(R.id.tvIdlingCount);
+
+        tvTPMS = (TextView) view.findViewById(R.id.tvTPMS);
+        tvTPMSPer = (TextView) view.findViewById(R.id.tvTPMSPer);
+        tvTPMSCount = (TextView) view.findViewById(R.id.tvTPMSCount);
+
+        tvCriticalWarning = (TextView) view.findViewById(R.id.tvCriticalWarning);
+        tvCriticalWarningPer = (TextView) view.findViewById(R.id.tvCriticalWarningPer);
+        tvCriticalWarningCount = (TextView) view.findViewById(R.id.tvCriticalWarningCount);
+
+        tvChecnkEngineOn = (TextView) view.findViewById(R.id.tvChecnkEngineOn);
+        tvChecnkEngineOnPer = (TextView) view.findViewById(R.id.tvChecnkEngineOnPer);
+        tvChecnkEngineOnCount = (TextView) view.findViewById(R.id.tvChecnkEngineOnCount);
+
+        tvSharpTurnLeft = (TextView) view.findViewById(R.id.tvSharpTurnLeft);
+        tvSharpTurnLeftPer = (TextView) view.findViewById(R.id.tvSharpTurnLeftPer);
+        tvSharpTurnLeftCount = (TextView) view.findViewById(R.id.tvSharpTurnLeftCount);
+
+        tvSharpTurnRight = (TextView) view.findViewById(R.id.tvSharpTurnRight);
+        tvSharpTurnRightPer = (TextView) view.findViewById(R.id.tvSharpTurnRightPer);
+        tvSharpTurnRightCount = (TextView) view.findViewById(R.id.tvSharpTurnRightCount);
+
+        tvHOS = (TextView) view.findViewById(R.id.tvHOS);
+        tvHOSPer = (TextView) view.findViewById(R.id.tvHOSPer);
+        tvHOSCount = (TextView) view.findViewById(R.id.tvHOSCount);
+
+        tvTripInspectionFailure = (TextView) view.findViewById(R.id.tvTripInspectionFailure);
+        tvTripInspectionFailurePer = (TextView) view.findViewById(R.id.tvTripInspectionFailurePer);
+        tvTripInspectionFailureCount = (TextView) view.findViewById(R.id.tvTripInspectionFailureCount);
+        
+        swMonthly = (CheckBox) view.findViewById(R.id.swMonthly);
+        swMonthly.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                isCurrentDate = !b;
+                ScoreCardGet(isCurrentDate);
+            }
+        });
+        lvScoreCard = (ListView) view.findViewById(R.id.lvScoreCard);
+        lvValues = (ListView) view.findViewById(R.id.lvValues);
+        tvDriving = (TextView) view.findViewById(R.id.tvDriving);
+        tvDeduction = (TextView) view.findViewById(R.id.tvDeduction);
+        tvTotalScores = (TextView) view.findViewById(R.id.tvTotalScores);
+
+        ScoreCardGet(isCurrentDate);
+        StartThread();
+
+
+    }
+
+    private void fillValues()
+    {
+        String date = Utility.getCurrentDate();
+        if (!isCurrentDate) {
+            date = Utility.getPreviousDate(-30);
+        }
+        int drivingMinute = HourOfServiceDB.DrivingTimeGet(date, Utility.onScreenUserId);
+        drivingScore = (drivingMinute / 10) * 5;
+        int deductedScore = 0;
+        list = AlertDB.getScoreCard(Utility.onScreenUserId, date);
+        for (AlertBean item : list) {
+            deductedScore += item.getScores();
+        }
+        int totalScores = drivingScore - deductedScore;
+    }
 
     private void StopThread() {
         if (thScores != null) {
@@ -159,27 +281,6 @@ public class ScoreCardFragment extends Fragment implements AlertDB.IScoreCard {
         tvTotalScores.setText("Net Scores: " + totalScores);
     }
 
-    private void initialize(View view) {
-        swMonthly = (CheckBox) view.findViewById(R.id.swMonthly);
-        swMonthly.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                isCurrentDate = !b;
-                ScoreCardGet(isCurrentDate);
-            }
-        });
-        lvScoreCard = (ListView) view.findViewById(R.id.lvScoreCard);
-        lvValues = (ListView) view.findViewById(R.id.lvValues);
-        tvDriving = (TextView) view.findViewById(R.id.tvDriving);
-        tvDeduction = (TextView) view.findViewById(R.id.tvDeduction);
-        tvTotalScores = (TextView) view.findViewById(R.id.tvTotalScores);
-
-        ScoreCardGet(isCurrentDate);
-        StartThread();
-
-    }
-
-
     private void initializeTab(View view) {
         TabHost host = (TabHost) view.findViewById(R.id.tabHost);
         host.setup();
@@ -193,7 +294,7 @@ public class ScoreCardFragment extends Fragment implements AlertDB.IScoreCard {
             }
         });
         //Tab 1
-        View  tabview = createTabView(host.getContext(), "Current");
+        View tabview = createTabView(host.getContext(), "Current");
         TabHost.TabSpec spec = host.newTabSpec("Current").setIndicator(tabview);
         spec.setContent(R.id.tabActive);
         host.addTab(spec);
